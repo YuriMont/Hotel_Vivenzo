f8863af4c66c37644d77f6b9747f19c5a42473cb
YuriMont <yuri.mont1043@gmail.com>
2023-12-07 14:34:10 -0300

    fix: update speed in verification action

--- pages/umboarding/parts/verification/verification.js
+++ pages/umboarding/parts/verification/verification.js
@@ -116,7 +116,7 @@ class VerificationWebComponent extends HTMLElement {
         .addEventListener("focusin", () => {
           setTimeout(() => {
             this.scrollToView();
-          }, 500);
+          }, 700);
         });
 
       this.shadowRoot
